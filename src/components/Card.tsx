b81827c4f62cbdffb4e0883df29bbec6af2cb58f
Stephan Gilli <gilli.stephan@gmail.com>
2021-09-03 20:36:18 +0200

    fix read more

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -64,7 +64,7 @@ export const Card = (props: CardProps) => {
           </Heading>
           <Box my={6}>
             <Box as={ImQuotesLeft} color="blue.500" fontSize="3xl" mb={3} />
-            <Text color="gray.600" minH="100px" noOfLines={4}>
+            <Text color="gray.600" minH="100px" noOfLines={5}>
               {text}
             </Text>
 
